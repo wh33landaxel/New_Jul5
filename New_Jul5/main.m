6a15458b5282171d0f6623f42067a49b2c96f831
wh33landaxel <xlnunez317@gmail.com>
2012-07-15 13:21:22 -0400

    Initial Commit

---
+++ New_Jul5/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  New_Jul5
+//
+//  Created by Axel Nunez on 7/15/12.
+//  Copyright (c) 2012 CUNY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GuitarAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GuitarAppDelegate class]));
+    }
+}
